0899687a023a779b3f86cda91297a9d89f2c61a1
avt <anand.takale@imgtec.com>
2016-11-16 06:45:38 +0000

    rebasing math functions

--- mathl/logbl.c
+++ mathl/logbl.c
@@ -31,69 +31,46 @@
  ******************************************************************************/
 
 /******************************************************************************
-* 		  file : $RCSfile: s_signbit.c,v $ 
-*     		author : $Author Imagination Technologies Ltd
-*    date last revised : $
-*      current version : $
-******************************************************************************/
+ *                 file : $RCSfile: logl.c,v $
+ *               author : $Author Imagination Technologies Ltd
+ *    date last revised : $
+ *      current version : $
+ ******************************************************************************/
 
-
-/* Copyright (C) 2002 by  Red Hat, Incorporated. All rights reserved.
- *
- * Permission to use, copy, modify, and distribute this software
- * is freely granted, provided that this notice is preserved.
- */
 /*
-FUNCTION
-<<signbit>>--Does floating-point number have negative sign?
+Copyright (c) 2014 Mentor Graphics, Inc.
+All rights reserved.
 
-INDEX
-	signbit
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+      * Neither the name of Mentor Graphics nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-ANSI_SYNOPSIS
-	#include <math.h>
-	int signbit(real-floating <[x]>);
-
-DESCRIPTION
-The <<signbit>> macro determines whether the sign of its argument value is
-negative.  The macro reports the sign of all values, including infinities,
-zeros, and NaNs.  If zero is unsigned, it is treated as positive.  As shown in
-the synopsis, the argument is "real-floating," meaning that any of the real
-floating-point types (float, double, etc.) may be given to it.
-
-Note that because of the possibilities of signed 0 and NaNs, the expression
-"<[x]> < 0.0" does not give the same result as <<signbit>> in all cases.
-
-RETURNS
-The <<signbit>> macro returns a nonzero value if and only if the sign of its
-argument value is negative.
-
-PORTABILITY
-C99, POSIX.
-
+THIS SOFTWARE IS PROVIDED BY CODESOURCERY, INC. ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL CODESOURCERY BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "low/_math.h"
+#include <math.h>
+#include "local.h"
 
-int __signbitf (float x);
-int __signbitd (double x);
-
-int
-__signbitf (float x)
+#ifdef _LDBL_EQ_DBL
+long double
+   logbl (long double x)
 {
-  __uint32_t w;
-
-  GET_FLOAT_WORD(w,x);
-
-  return (w>>31);
+  return logb(x);
 }
-
-int
-__signbitd (double x)
-{
-  __uint32_t msw;
-
-  GET_HIGH_WORD(msw, x);
-
-  return (msw>>31);
-}
+#endif
