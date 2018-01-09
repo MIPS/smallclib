bb5137ff9221657a0eb25d810530bba779d928f8
Matthew Fortune <matthew.fortune@imgtec.com>
2018-01-09 10:52:58 +0000

    Update copyright year to 2018 and owner to MIPS Tech, LLC

--- toolchains/ToolChainMIPS.cmake
+++ toolchains/ToolChainMIPS.cmake
@@ -1,9 +1,7 @@
 
 ###############################################################################
 #
-# Copyright 2014-2015, Imagination Technologies Limited and/or its
-#                      affiliated group companies.
-# All rights reserved.
+# Copyright (C) 2014-2018 MIPS Tech, LLC
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -33,7 +31,6 @@
 
 ###############################################################################
 #              file : $RCSfile: ToolChainMIPS.txt,v $
-#            author : $Author: Imagination Technologies Ltd
 # date last revised : 
 #   current version : 
 ###############################################################################
