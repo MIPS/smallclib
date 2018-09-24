d3caedef6ca2f0e8ff75064500d68667a11586bd
Faraz Shahbazker <fshahbazker@wavecomp.com>
2018-09-24 14:08:43 -0700

    Fix missing comment markers in previous commit

--- stdlib/on_exit.c
+++ stdlib/on_exit.c
@@ -28,6 +28,7 @@
  *
  ******************************************************************************/
 
+/*
  * Copyright (c) 1990 Regents of the University of California.
  * All rights reserved.
  *
